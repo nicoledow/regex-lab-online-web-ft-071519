998aeceace25b34bf740b40aa9d24307e38c1c94
Nicole Dow <nikki.dow71@gmail.com>
2019-07-17 22:44:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -35,5 +35,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone =~ /\d[0-9-]{
+  phone =~ /\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/ ? true : false
 end
