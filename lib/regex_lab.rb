f50ed8f6c53b76e51320968c4d82a4ba5cb27107
Nicole Dow <nikki.dow71@gmail.com>
2019-07-17 22:44:46 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,39 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word[0] =~ /[aeiou]/i ? true : false
 end
+
 
 def words_starting_with_un_and_ending_with_ing(text)
 
+  array_of_words = text.split(" ")
+  un_ing_words = []
+  array_of_words.each do |word|
+    if word[0..1] =~ /un/i && word[-3..-1] =~ /ing/i
+      un_ing_words << word
+    end
+  end
+un_ing_words
 end
+
 
 def words_five_letters_long(text)
-
+  have_5_letters = []
+  
+  text.split(" ").each do |word|
+    if word.length == 5
+      have_5_letters << word
+    end
+  end
+  have_5_letters
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
 
+def first_word_capitalized_and_ends_with_punctuation?(text)
+   (text[0] =~ /[A-Z]/) && (text[-1] =~ /[!.?]/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone =~ /\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/ ? true : false
 end
